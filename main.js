59b669054c00cfba397b86ca309c4d34e2237db4
rdanka <58121543+rdanka@users.noreply.github.com>
2020-04-04 22:41:48 +0200

    SelectionSort
    selection sort added

--- main.js
+++ main.js
@@ -2,45 +2,28 @@ const canvas = document.querySelector(".canvas");
 const generateArrayButton = document.querySelector("#generateArray");
 const currentValueText = document.querySelector("#currentValue");
 const timerValue = document.querySelector(".timer");
-
-const selectionSortButton = document.querySelector("#selectionSort");
-
 const sizeSlider = document.querySelector("#slider");
-
 let arraySize = 50;
 let time = 0;
 let arr = [];
-
-
-
-
 
 sizeSlider.addEventListener('input', () => {
     arraySize = sizeSlider.value;
     currentValueText.innerText = sizeSlider.value;
 });
 
-
 timerValue.addEventListener('input', () => {
     time = timerValue.value;
 });
 
-
 generateArrayButton.addEventListener('click', () => {
     arr = generateArray(arraySize);
-    console.log(arr);
     draw(arr);
 });
 
-selectionSortButton.addEventListener('click', () => {
-    arr = selectionSort(arr);
-    console.log(arr);
-    draw(arr);
-});
 
 
 function generateArray(size) {
-    let arr = [];
     for (let i = 0; i < size; i++) {
         arr[i] = Math.floor(Math.random() * 500 + 1);
     }
@@ -66,12 +49,13 @@ function swap(arr, j, i) {
 
 }
 
-/* Sorting Algorithms */
-
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+/* Sorting Algorithms */
+
+
 
 let swaps = 0;
 
@@ -117,8 +101,8 @@ async function bubbleSort() {
             columns[animation.indexI].style.backgroundColor = "red";
             columns[animation.indexJ].style.backgroundColor = "green";
         } else {
-            columns[animation.indexI].style.backgroundColor = "red";
-            columns[animation.indexJ].style.backgroundColor = "green";
+            columns[animation.indexI].style.backgroundColor = "green";
+            columns[animation.indexJ].style.backgroundColor = "red";
             await sleep(time);
             const tempHeight = columns[animation.indexI].style.height;
             columns[animation.indexI].style.height = columns[animation.indexJ].style.height;
@@ -134,21 +118,77 @@ async function bubbleSort() {
     }
 }
 
-function selectionSort(arr) {
-    let n = arr.length;
-    let minIndex;
-    for (i = 0; i < n - 1; i++) {
-        minIndex = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-                swaps++;
+
+
+function getSelectionSortAnimations(arr) {
+    let animatedArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        animatedArray.push({
+            min: i,
+            status: 'select-min',
+        });
+        for (let j = i + 1; j < arr.length; j++) {
+            animatedArray.push({
+                min,
+                j,
+                status: 'compare'
+            });
+            if (arr[min] > arr[j]) {
+                animatedArray.push({
+                    min: j,
+                    status: 'select-min',
+                });
+                min = j;
             }
         }
-        let temp = arr[minIndex];
-        arr[minIndex] = arr[i];
-        arr[i] = temp;
+
+        if (min !== i) {
+            animatedArray.push({
+                min,
+                i,
+                status: 'swap'
+            });
+            swap(arr, i, min);
+        }
     }
-    console.log(`The Selection Sort did ${swaps} comparisons.`);
-    return arr;
+    return animatedArray;
+}
+
+async function selectionSort() {
+    let columns = Array.from(canvas.children);
+    let animatedArray = getSelectionSortAnimations(arr);
+    let previous;
+
+    for (let i = 0; i < animatedArray.length; ++i) {
+        await sleep(time);
+        if (previous) {
+            Number.isInteger(previous.min) && (columns[previous.min].style.backgroundColor = "white");
+            Number.isInteger(previous.j) && (columns[previous.j].style.backgroundColor = "white");
+            Number.isInteger(previous.i) && (columns[previous.i].style.backgroundColor = "white");
+        }
+        const animation = animatedArray[i];
+
+        if (animation.status === 'compare') {
+            columns[animation.min].style.backgroundColor = "green";
+            columns[animation.j].style.backgroundColor = "green";
+        } else if (animation.status === 'swap') {
+            columns[animation.min].style.backgroundColor = "red";
+            columns[animation.i].style.backgroundColor = "red";
+            await sleep(time);
+            const tempHeight = columns[animation.min].style.height;
+            columns[animation.min].style.height = columns[animation.i].style.height;
+            columns[animation.i].style.height = tempHeight;
+        } else {
+            columns[animation.min].style.backgroundColor = "yellow";
+        }
+
+        previous = animation;
+    }
+
+    if (previous) {
+        Number.isInteger(previous.min) && (columns[previous.min].style.backgroundColor = "white");
+        Number.isInteger(previous.j) && (columns[previous.j].style.backgroundColor = "white");
+        Number.isInteger(previous.i) && (columns[previous.i].style.backgroundColor = "white");
+    }
 }
